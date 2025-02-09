b09867d7e5947c13f7e79d18da8f7995aa513e97
Kyle Woodward <btwood88@gmail.com>
2025-02-09 01:29:58 -0600

    webp fix 2

--- next.config.mjs
+++ next.config.mjs
@@ -3,6 +3,7 @@ const isProd = process.env.NODE_ENV === 'production';
 const nextConfig = {
   output: "export",
   basePath: isProd ? '/tli-builder' : '',
+  assetPrefix: isProd ? '/tli-builder' : '',
   reactStrictMode: false,
   images: { unoptimized: true, formats: ['image/webp'], },
   eslint: {
